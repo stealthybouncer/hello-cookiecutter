9a05944a431868c22b367c450c7f1acad6f35083
Francis Rodrigues <francis.n.rodrigues@gmail.com>
2024-03-02 22:39:08 -0500

    toml

---
+++ {{cookiecutter.project_slug}}/app.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for license information.
+#-----------------------------------------------------------------------------------------
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return app.send_static_file("index.html")   
